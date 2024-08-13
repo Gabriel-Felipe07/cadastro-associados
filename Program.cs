5e301cceb8e0562e3cbcc3f755c4b43bc9229572
Gabriel Belentani Felipe <gabriel.belentani217@gmail.com>
2024-08-13 08:59:32 -0300

    Comit inicial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace lista_dicionario_csharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> cadastro_de_nomes = new List<string>();
+        string names = "";
+        int escolha;
+        while(true){
+            Console.WriteLine("++++++++++++++++++++++++++++++");
+            Console.WriteLine("+        Clube do C#+        +");
+            Console.WriteLine("++++++++++++++++++++++++++++++");
+            Console.WriteLine("+        1- Cadastrar        +");
+            Console.WriteLine("+   2- Mostrar associados    +");
+            Console.WriteLine("+          0- Sair           +");
+            Console.WriteLine("++++++++++++++++++++++++++++++");
+            escolha = Convert.ToInt32(Console.ReadLine());
+            
+            if(escolha == 1){
+                Console.WriteLine("Cadastro de associado\r\n");
+                Console.WriteLine("Digite (stop) para parar");
+
+                while(names != "stop"){
+                    names = Console.ReadLine()!;
+                    cadastro_de_nomes.Add(names);
+                }
+            }else if(escolha == 2){
+                Console.WriteLine("Buscando lista de associados...");
+                cadastro_de_nomes.RemoveAll(x => x.ToLower() == "stop");
+                Thread.Sleep(1000);
+                foreach(var nome in cadastro_de_nomes){
+                    Console.WriteLine(nome);
+                }
+            }else if(escolha == 0){
+                Console.WriteLine("Obrigado por participar!");
+                break;
+            }
+        }
+    }
+}
